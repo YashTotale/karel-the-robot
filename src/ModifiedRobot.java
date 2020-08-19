644f6e2d29bd7f349fb6852a0b189150c0cdb5c5
YashTotale <totaleyash@gmail.com>
2020-08-18 17:37:19 -0700

    ModifiedRobot working

--- src/ModifiedRobot.java
+++ src/ModifiedRobot.java
@@ -5,8 +5,6 @@ public class ModifiedRobot extends Robot implements Directions {
         super(street, avenue, facing, beepers);
     }
 
-
-
     public void go(int times) {
         for (int i = 0; i < times; i++) {
             move();
@@ -20,15 +18,41 @@ public class ModifiedRobot extends Robot implements Directions {
     }
 
     public void turnTowards(Direction towards) {
-
+        int currentVal = getDirectionVal(direction());
+        int towardsVal = getDirectionVal(towards);
+        System.out.println(towardsVal);
+        int turns = currentVal - towardsVal;
+        if(turns < 0) {
+            turns+=4;
+        }
+        turn(turns);
     }
 
-    public void moveDiagonal(int times, Direction[] directions) {
+    public void moveDiagonal(int times, Direction[] directions, boolean withBeeper) {
         if (directions.length != 2) {
-            throw new IllegalArgumentException("directions must be of length 2");
+            throw new IllegalArgumentException("Exactly 2 directions must be passed in");
         }
         for (int i = 0; i < times; i++) {
-            System.out.println(i);
+            for(int x = 0; x < directions.length; x++) {
+                turnTowards(directions[x]);
+                move();
+            }
+            if(withBeeper) {
+                putBeeper();
+            }
         }
+    }
+
+    private static int getDirectionVal(Direction direction) {
+        if (direction == East) {
+            return EastVal;
+        }
+        if (direction == South) {
+            return SouthVal;
+        }
+        if (direction == West) {
+            return WestVal;
+        }
+        return NorthVal;
     }
 }
--- src/RobotStarter.java
+++ src/RobotStarter.java
@@ -6,67 +6,21 @@ import kareltherobot.World;
 
 public class RobotStarter implements Directions {
 
-    private static int length = 5;
+    private static int length = 30;
 
     public static void main(String[] args) {
         //Create our robot
         ModifiedRobot bot = new ModifiedRobot(length, 1, East, length * 4 - 3);
         //Configure World
-        World.setVisible(true);
-        World.setSize(length * 3, length * 3);
-        World.showSpeedControl(true);
+        World.setVisible(true); //Allows us to see the world
+        World.setSize(length * 3, length * 3); //Sets the size of the world
+        World.showSpeedControl(true); //Shows a slider that controls the speed
+        //World.setDelay(0) // <- Enable when you want the finished result as soon as you run the program
 
         bot.putBeeper();
-        moveDiagonal(North, East, length -1, true, bot);
-        moveDiagonal(South, East, length -1, true, bot);
-        moveDiagonal(South, West, length -1, true, bot);
-        moveDiagonal(North, West, length -1, true, bot);
-    }
-
-    private static void turn(int times, Robot bot) {
-        for (int i = 0; i < times; i++) {
-            bot.turnLeft();
-        }
-    }
-
-    private static void move(int times, Robot bot) {
-        for (int i = 0; i < times; i++) {
-            bot.move();
-        }
-    }
-
-    private static void turnTowards(Direction towards, Robot bot) {
-        int turns = getTurnsTowardsDirection(getDirection(bot), towards);
-        turn(turns, bot);
-    }
-
-    private static void moveDiagonal(Direction vertically, Direction horizontally, int times, boolean withBeepers, Robot bot) {
-        for (int i = 0; i < times; i++) {
-            turnTowards(vertically, bot);
-            move(1, bot);
-            turnTowards(horizontally, bot);
-            move(1, bot);
-            if (withBeepers) {
-                bot.putBeeper();
-            }
-        }
-    }
-
-    private static Direction getDirection(Robot bot) {
-        return bot.facingNorth() ? North : bot.facingSouth() ? South : bot.facingEast() ? East : West;
-    }
-
-    private static int getIntValForDirection(Direction direction) {
-        return East.equals(direction) ? 0 : North.equals(direction) ? 1 : West.equals(direction) ? 2 : 3;
-    }
-
-    private static int getTurnsTowardsDirection(Direction current, Direction towards) {
-        int currentInt = getIntValForDirection(current);
-        int towardsInt = getIntValForDirection(towards);
-        int difference = towardsInt - currentInt;
-        if (difference >= 0) {
-            return difference;
-        }
-        return 4 + difference;
+        bot.moveDiagonal(length -1, new Direction[]{North, East}, true);
+        bot.moveDiagonal(length -1, new Direction[]{South, East}, true);
+        bot.moveDiagonal(length -1, new Direction[]{South, West}, true);
+        bot.moveDiagonal(length -1, new Direction[]{North, West}, true);
     }
 }
