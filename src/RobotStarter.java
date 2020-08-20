020e5d98cdf225fa0979643fd4ad116dc41fd756
YashTotale <totaleyash@gmail.com>
2020-08-20 15:58:50 -0700

    added user input for length

--- src/RobotStarter.java
+++ src/RobotStarter.java
@@ -1,16 +1,22 @@
 import kareltherobot.Directions;
 import kareltherobot.World;
+import java.util.Scanner;
 
 public class RobotStarter implements Directions {
 
     /**
      * The length of each side of the diamond
      */
-    private static final int length = 10;
+    private static int length = 10;
 
     public static void main(String[] args) {
+        System.out.println("Enter the size of the diamond you want: ");
+        Scanner keyboard = new Scanner(System.in);
+        length = keyboard.nextInt();
+        System.out.println("You selected: " + length);
+
         //Create our robot
-        ModifiedRobot bot = new ModifiedRobot(length, length, North, length * 4 - 3);
+        ModifiedRobot bot = new ModifiedRobot(length, 1, North, length * 4 - 3);
         //Configure World
         World.setVisible(true); //Allows us to see the world
         World.setSize(length * 3, length * 3); //Sets the size of the world
